ee7ee99aeb48040f3dbc09a8e95133955cba3eae
Raph Cohn <raphael.cohn@stormmq.com>
2015-10-21 16:35:39 +0100

    Consistency

--- lib/pallene/modules/banias/init.lua
+++ lib/pallene/modules/banias/init.lua
@@ -3,20 +3,3 @@ This file is part of banias. It is subject to the licence terms in the COPYRIGHT
 Copyright © 2015 The developers of banias. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/raphaelcohn/banias/master/COPYRIGHT.
 ]]--
 
-
-local assert = require('halimede').assert
-local exception = require('halimede.exception')
-
-
-assert.globalTableHasChieldFieldOfTypeFunction('os', 'getenv')
-assert.globalTypeIsFunction('require')
-local function loadWriter()
-	local environmentVariable = 'LUA_BANIAS_WRITER'
-	local writer = os.getenv(environmentVariable)
-	if writer == nil then
-		exception.throw("The environment variable '%s' is not set", environmentVariable)
-	end
-	require('banias.writers.' .. writer)
-end
-
-loadWriter()
--- lib/pallene/modules/banias/writers/init.lua
+++ lib/pallene/modules/banias/writers/init.lua
@@ -4,3 +4,19 @@ Copyright © 2015 The developers of banias. See the COPYRIGHT file in the top-le
 ]]--
 
 
+local assert = require('halimede').assert
+local exception = require('halimede.exception')
+
+
+assert.globalTableHasChieldFieldOfTypeFunction('os', 'getenv')
+assert.globalTypeIsFunction('require')
+local function loadWriter()
+	local environmentVariable = 'LUA_BANIAS_WRITER'
+	local writer = os.getenv(environmentVariable)
+	if writer == nil then
+		exception.throw("The environment variable '%s' is not set", environmentVariable)
+	end
+	require('banias.writers.' .. writer)
+end
+
+loadWriter()
